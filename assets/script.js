be21c90636a97d24bb6f8c9cf5c06c66220a7247
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-04-16 13:34:43 -0400

    addressing onload issue

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,33 @@
 //setting up variables
 var generateBtn = document.querySelector("#generate");
-var lowercaseAlphabet = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
-var uppercaseAlphabet = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"; 
-var numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9, 0";
-var symbols = "1, @, #, $, %, ^, &, *, (, ), _, ?. /, ;, :, <, >, [, ], {, },";
+var lowercaseAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var uppercaseAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '?', '/', ';', ':', '<', '>', '[', ']', '{', '}']
+
 var totalCharacterBank = [];
 var resultPassword = "";
 
 //Setting up the way the page looks when it loads
-window.onload = alert("Create a secure password! Click the button to begin, then select your parameters to generate your new password.");
+// window.onload = alert("Create a secure password! Click the button to begin, then select your parameters to generate your new password.");
 
-//Then the person clicks the button to begin
+//Then the person begins
 function beginPasswordCreation(){
   var password = makePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password; 
+
 }
 
-//Asking user to choose parameters
-var passwordLength = prompt("How many characters should the password include? Select a number between 8 and 128 characters.")
+function makePassword(){
+  alert("Create a secure password! Click the button to begin, then select your parameters to generate your new password.");
+  totalCharacterBank = [];
+  resultPassword = "";  
+//}
+
+//Asking user to choose parameters--everything they choose is added to the main character bank to draw from later
+
+var passwordLength = window.prompt("How many characters should the password include? Select a number between 8 and 128 characters.")
 
 if(passwordLength <8 || passwordLength > 128){
   alert("Select a number between 8 and 128 characters.");
@@ -26,35 +35,41 @@ if(passwordLength <8 || passwordLength > 128){
   else{
     if(confirm("Should the password contain lower case letters?")){
       Array.prototype.push.apply(totalCharacterBank, [lowercaseAlphabet])
+      console.log("lower case added" + " " + totalCharacterBank)
+      
   }
   if(confirm("Should the password contain upper case letters?")){
       Array.prototype.push.apply(totalCharacterBank, [uppercaseAlphabet])
+      console.log("upper case added" + " " + totalCharacterBank)
   }
   if(confirm("Should the password contain special characters?")){
       Array.prototype.push.apply(totalCharacterBank, [symbols])
+      console.log("special added" + " " + totalCharacterBank)
   }
   if(confirm("Should the password contain numbers?")){
       Array.prototype.push.apply(totalCharacterBank, [numbers])
+      console.log("numbers added" + " " + totalCharacterBank)
+  }
+  console.log("bank at end of function" + " " + totalCharacterBank)
   }
 
   //Randomizing
-  else{
-    for(var i=0; i<passwordLength; i++){
-      var randomize = Math.floor(Math.random()*totalCharacterBank.length);
-      resultPassword += totalCharacterBank[random]; 
-    }
+    // for(var i=0; i<passwordLength; i++){
+    //   var random= Math.floor(Math.random()*totalCharacterBank.length);
+    //   resultPassword = totalCharacterBank[random]; 
+    // }
+  shuffle(totalCharacterBank)
+    console.log(totalCharacterBank)
+
+  console.log(resultPassword)
     //Result
     document.getElementById("password").innerHTML = resultPassword; 
-  }
-}
-
-function makePassword(){
-  var totalCharacterBank = [];
-  resultPassword = "";  
-}
 
 //Making it work on click
 generateBtn.addEventListener("click", beginPasswordCreation);
-
+}
 //Calliing the function
-makePassword();
+// makePassword();
+generateBtn.addEventListener("click", makePassword);
+
+
